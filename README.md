9a2c1746ed2f0cfc3ce9b21d69aec221318708d7
Changes729 <changes729@163.com>
2024-03-01 17:00:27 +0800

    添加 kicanvas 案例

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ npm install cnpm -g  --registry=https://registry.npm.taobao.org
 cnpm install --save-dev @electron-forge/cli
 cnpm exec --package=@electron-forge/cli -c "electron-forge import"
 ```
+
+## Examples
+
+- [kicanvas](https://github.com/theacodes/kicanvas): [install here](https://kicanvas.org/embedding/#installation)
--- script/esbuild.mjs
+++ script/esbuild.mjs
@@ -2,10 +2,14 @@ import * as esbuild from "esbuild";
 import fs from "node:fs";
 
 const HTML_DIR = "public/html/";
+const JS_DIR = "public/js/";
+const STATIC_DIR = "public/static/";
 const OUT_DIR = "dist/";
 
 fs.rmSync(OUT_DIR, { recursive: true, force: true });
 fs.cpSync(HTML_DIR, OUT_DIR, {recursive: true});
+fs.cpSync(JS_DIR, `${OUT_DIR}/js`, {recursive: true});
+fs.cpSync(STATIC_DIR, `${OUT_DIR}/static`, {recursive: true});
 
 let ctx = await esbuild.context({
   entryPoints: ["src/app.jsx"],
